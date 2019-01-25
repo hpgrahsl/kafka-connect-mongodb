3dcc8f95e9eb1c15db95fa3728af62fda265e6d9
hpgrahsl <hpgrahsl@users.noreply.github.com>
2019-01-25 14:35:32 +0100

    fix StringUtils dependency issue

--- src/main/java/at/grahsl/kafka/connect/mongodb/MongoDbSinkTask.java
+++ src/main/java/at/grahsl/kafka/connect/mongodb/MongoDbSinkTask.java
@@ -30,7 +30,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.BulkWriteOptions;
 import com.mongodb.client.model.WriteModel;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.connect.errors.ConnectException;
