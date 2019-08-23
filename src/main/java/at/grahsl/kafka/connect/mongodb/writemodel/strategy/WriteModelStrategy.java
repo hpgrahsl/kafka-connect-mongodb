9e3d47092b62021ddefbd7b90f000efcbbd28758
hpgrahsl <hpgrahsl@users.noreply.github.com>
2019-08-23 16:48:43 +0200

    preliminary work to support monotonic writes to avoid updates with stale data

--- src/main/java/at/grahsl/kafka/connect/mongodb/writemodel/strategy/WriteModelStrategy.java
+++ src/main/java/at/grahsl/kafka/connect/mongodb/writemodel/strategy/WriteModelStrategy.java
@@ -2,10 +2,15 @@ package at.grahsl.kafka.connect.mongodb.writemodel.strategy;
 
 import at.grahsl.kafka.connect.mongodb.converter.SinkDocument;
 import com.mongodb.client.model.WriteModel;
+import org.apache.kafka.connect.sink.SinkRecord;
 import org.bson.BsonDocument;
 
 public interface WriteModelStrategy {
 
     WriteModel<BsonDocument> createWriteModel(SinkDocument document);
+
+    default WriteModel<BsonDocument> createWriteModel(SinkDocument document, SinkRecord record) {
+        return createWriteModel(document);
+    }
 
 }
